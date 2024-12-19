4798c384c9afd71cfaae34fb1ff0643cc497e5f6
mirko <mirkoriek-02@web.de>
2024-12-19 10:18:43 +0100

    Refined the looks of the main menu

--- Scripts/main_menu.gd
+++ Scripts/main_menu.gd
@@ -1,11 +1,14 @@
 extends Control
 
-
+func _process(delta: float) -> void:
+	if Globals.MAIN_MENU:
+		$Background/SildeCam.position.x += 1
 
 func _on_play_pressed() -> void:
 	var level = preload("res://Scenes/level_1.tscn").instantiate(PackedScene.GEN_EDIT_STATE_DISABLED)
 	get_node("../../").add_child(level)
 	hide()
+	$Background/SildeCam.enabled = false
 	var intro_text = [
 		"Du bist ein Schneemann und deine Schneefrau wurde brutal von\n
 		Schrergen des wild gewordenen Weihnachtsmannes getötet.",
@@ -17,8 +20,13 @@ func _on_play_pressed() -> void:
 	get_parent().start_text_sequence(intro_text)
 
 func _on_options_pressed() -> void:
-	pass # Replace with function body.
-
+	pass
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
+
+func credits() -> void:
+	pass
+
+func new_game() -> void:
+	pass
